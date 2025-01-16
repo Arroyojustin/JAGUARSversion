2b4b1af8055b2501337fffc2f50c00c217313cf2
Arroyojustin <159241131+Arroyojustin@users.noreply.github.com>
2025-01-16 10:01:07 +0800

    update database

--- db/sports_management.sql
+++ db/sports_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2025 at 04:05 AM
+-- Generation Time: Jan 16, 2025 at 03:00 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,23 +39,8 @@ CREATE TABLE `approvals` (
   `bmi` decimal(5,2) NOT NULL,
   `phone_number` varchar(15) NOT NULL,
   `health_protocol` text DEFAULT NULL,
-  `approved_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` enum('approved','rejected') NOT NULL DEFAULT 'approved'
+  `approved_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `approvals`
---
-
-INSERT INTO `approvals` (`id`, `first_name`, `middle_initial`, `last_name`, `gender`, `sport_id`, `height`, `weight`, `bmi`, `phone_number`, `health_protocol`, `approved_at`, `status`) VALUES
-(37, 'MA Concepcion', '.', 'Arroyo', '', 17, 165.00, 55.00, 20.20, '0935252525252', 'Mataba', '2025-01-10 05:47:52', 'approved'),
-(40, 'Stephen', 'W', 'Curry', '', 16, 0.00, 0.00, 0.00, '09369007677', 'wala lang', '2025-01-10 01:08:41', 'approved'),
-(41, 'Kyrie', 'D', 'Irving', '', 19, 0.00, 0.00, 0.00, '0906837106', 'None', '2025-01-10 00:31:36', 'approved'),
-(44, 'Justin', 'M', 'Arroyo', '', 16, 0.00, 0.00, 0.00, '09068377106', 'None', '2025-01-10 05:47:32', 'approved'),
-(46, 'Lebron', 'J', 'James', '', 16, 0.00, 0.00, 0.00, '0952355221', 'none', '2025-01-11 07:31:50', 'approved'),
-(60, 'Albert', 'D', 'Alias', '', 16, 0.00, 0.00, 0.00, '09314141324', 'none', '2025-01-13 15:26:48', 'approved'),
-(62, 'son', 'D', 'goku', '', 16, 0.00, 0.00, 0.00, '09778768755', 'none', '2025-01-14 00:17:16', 'approved'),
-(65, 'Nahoya', 'T', 'Kawata', '', 16, 0.00, 0.00, 0.00, '0948872522', 'none', '2025-01-14 00:31:38', 'approved');
 
 -- --------------------------------------------------------
 
@@ -108,30 +93,7 @@ CREATE TABLE `requirements` (
 --
 
 INSERT INTO `requirements` (`id`, `first_name`, `middle_initial`, `last_name`, `gender`, `sport_id`, `height`, `weight`, `bmi`, `phone_number`, `health_protocol`, `status`) VALUES
-(37, 'MA Concepcion', '.', 'Arroyo', 'female', 17, 165.00, 55.00, 20.20, '0935252525252', 'Mataba', 'pending'),
-(40, 'Stephen', 'W', 'Curry', 'male', 16, 0.00, 0.00, 0.00, '09369007677', 'wala lang', 'pending'),
-(41, 'Kyrie', 'D', 'Irving', 'male', 19, 0.00, 0.00, 0.00, '0906837106', 'None', 'pending'),
-(44, 'Justin', 'M', 'Arroyo', 'male', 16, 0.00, 0.00, 0.00, '09068377106', 'None', 'pending'),
-(45, 'Bryan', 'g', 'Custodio', 'male', 16, 0.00, 0.00, 0.00, '09068377106', 'None', 'pending'),
-(46, 'Lebron', 'J', 'James', 'male', 16, 0.00, 0.00, 0.00, '0952355221', 'none', 'pending'),
-(47, 'Tom ', '&', 'Jerry', 'male', 16, 0.00, 0.00, 0.00, '09588732525', 'none', 'pending'),
-(48, 'Aron Cyrus', 'D', 'Esteron', 'male', 17, 0.00, 0.00, 0.00, '098777625765', 'none', 'pending'),
-(49, 'Jedan Voltaire', 'A', 'Esteron', 'male', 18, 0.00, 0.00, 0.00, '0944363637', 'none', 'pending'),
-(50, 'Mark Dave', 'A', 'Antonio', 'male', 18, 0.00, 0.00, 0.00, '09651446321', 'none', 'pending'),
-(51, 'Alan ', 'D', 'Walker', 'male', 19, 0.00, 0.00, 0.00, '0921435566', 'nonee', 'pending'),
-(52, 'Welcome ', 'S', 'Back', 'male', 18, 0.00, 0.00, 0.00, '09435543654', 'None', 'pending'),
-(53, 'pepsi ', 'V', 'paloma', 'female', 17, 0.00, 0.00, 0.00, '094353252', 'none', 'pending'),
-(54, 'Iron', 'R', 'Man', 'male', 19, 0.00, 0.00, 0.00, '0945474776', 'patay na', 'pending'),
-(55, 'Mountain', 'S', 'Dew', 'male', 18, 0.00, 0.00, 0.00, '09444444444', 'matamis', 'pending'),
-(57, 'Wardell', 'C', 'Gomez', 'male', 16, 0.00, 0.00, 0.00, '0982342114132', 'None', 'pending'),
-(58, 'Archie ', 'A', 'Nicer', 'male', 16, 0.00, 0.00, 0.00, '09887378623', 'none', 'pending'),
-(59, 'Catrina', 'A', 'Lumor', 'female', 18, 0.00, 0.00, 0.00, '0938763876', 'None', 'pending'),
-(60, 'Albert', 'D', 'Alias', 'male', 16, 0.00, 0.00, 0.00, '09314141324', 'none', 'pending'),
-(61, 'Jenith ', 'A', 'Diniega', 'female', 17, 0.00, 0.00, 0.00, '092342545', 'none', 'pending'),
-(62, 'son', 'D', 'goku', 'male', 16, 0.00, 0.00, 0.00, '09778768755', 'none', 'pending'),
-(63, 'Hanamichi', 'S', 'Sakuragi', 'male', 16, 0.00, 0.00, 0.00, '098768232', 'none', 'pending'),
-(64, 'Kaide', 'A', 'Rukawa', 'male', 16, 0.00, 0.00, 0.00, '09876557657', 'none', 'pending'),
-(65, 'Nahoya', 'T', 'Kawata', 'male', 16, 0.00, 0.00, 0.00, '0948872522', 'none', 'pending');
+(80, 'George', 'D', 'Balauag', 'male', 18, 0.00, 0.00, 0.00, '0987876612', 'none', 'pending');
 
 -- --------------------------------------------------------
 
@@ -194,8 +156,8 @@ INSERT INTO `users` (`id`, `lastname`, `firstname`, `middle_initial`, `student_n
 (25, 'Esteron', 'Aron', 'D', NULL, NULL, NULL, NULL, NULL, '097264872622', 'aron@edu.ph', '$2y$10$I1mP8ux7UvH1drW0x4ZL1uiZPOdA5qP/9m7L6/hI7Nd1CmMybi0Se', 'coordinator', 'male', 'single', NULL, NULL),
 (26, 'Kerr', 'Steve', 'S', NULL, NULL, NULL, NULL, NULL, '09422323233', 'gsw@edu.ph', '$2y$10$tUtrjvtw6GaZoM0..qZfU.A0W2I47pdBldq59w/ODKob2Qb1UBPTa', 'coach', 'male', NULL, NULL, 16),
 (29, 'hernandez', 'Tania', 'A', NULL, NULL, NULL, NULL, NULL, '092323232323', 'taniii@edu.ph', '$2y$10$ghJdZAjoPAKAVsDd6C51feo2oEoF7Q9vNvdgFmGNkkWHnlQICfUz6', 'coach', 'female', NULL, NULL, 17),
-(38, 'Curry', 'Stephen', NULL, '1-210134', NULL, NULL, NULL, NULL, NULL, '1-210134@edu.ph', '$2y$10$1ZfZLlwR8g/1/KN2ocsVJO0nljqNLbwgDXpTJsGp.3nQvBG0VQ8di', 'student', NULL, NULL, NULL, 16),
-(39, 'Arroyo', 'MA Concepcion', NULL, '1-210136', NULL, NULL, NULL, NULL, NULL, '1-210136@edu.ph', '$2y$10$LYOWb.hhCHbffogD4A7sIejVrEDR.qYOHqNPTlD5t.vzQ5040iVYW', 'student', NULL, NULL, NULL, 17);
+(55, 'Payos', 'John Paul ', 'R', '1-210134', 0.00, 0.00, 0.00, NULL, '09987876833', '1-210134@edu.ph', '$2y$10$8qmOXHqriaQ1CsNWHxlwwO5cYcmqVklHu6.ipbMZfDL8GCSwfDH9a', 'student', 'male', NULL, NULL, 16),
+(56, 'Custodio', 'Bryan', 'G', '1-210136', 0.00, 0.00, 0.00, NULL, '09888888888', '1-210136@edu.ph', '$2y$10$kR0qcZU3cmmG0TVQ1ehrquy4C505Be1wdL0yG7gtRdR2Wxox3/huC', 'student', 'male', NULL, NULL, 16);
 
 --
 -- Triggers `users`
@@ -255,7 +217,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `approvals`
 --
 ALTER TABLE `approvals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `coaches`
@@ -267,7 +229,7 @@ ALTER TABLE `coaches`
 -- AUTO_INCREMENT for table `requirements`
 --
 ALTER TABLE `requirements`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `sports`
@@ -279,7 +241,7 @@ ALTER TABLE `sports`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- Constraints for dumped tables
